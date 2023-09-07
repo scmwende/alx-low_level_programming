8bdc5dfbf1526dfd846e6314dffcebbf1ed47529
scmwende <stellacarolinemwendenjeru@gmail.com>
2023-09-07 08:59:14 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Prints string using puts function
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
