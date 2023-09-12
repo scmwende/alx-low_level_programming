f3e5f409baa3ae7de0259754580dbfc6342f4265
scmwende <stellacarolinemwendenjeru@gmail.com>
2023-09-12 07:55:08 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+Project 214 0x02. C - Funtions, nested loops
+C
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar - Writes a character to the standard output (stdout)
+ * @c: The character to be written
+ * Return: On success, the number of characters written is returned
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
