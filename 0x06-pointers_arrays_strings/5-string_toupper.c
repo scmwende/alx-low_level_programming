60ce9c5f56d2e224160719b830ea506b79b57260
scmwende <stellacarolinemwendenjeru@gmail.com>
2023-09-20 13:01:33 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Converts all lowercase letters of a string to uppercase
+ * @str: The string to convert
+ * Return: A pointer to the resulting string
+ */
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
